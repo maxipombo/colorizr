b42a52ba654cff2caa0a3c49c7ef982e32e4b1ad
Maximiliano Pombo <maxipombo@gmail.com>
2016-03-05 16:44:56 -0300

    Add colors

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,1 +1,24 @@
+class String
+  COLORS = {
+    red: 31,
+    green: 32,
+    yellow: 33,
+    blue: 34,
+    pink: 35,
+    light_blue: 94,
+    white: 97,
+    light_grey: 37,
+    black: 30
+  }
 
+  def self.create_colors
+    COLORS.each do |color, code|
+      send(:define_method, "#{color}") do
+        "\e[#{code}m#{self}\e[0m"
+      end
+    end
+  end
+
+end
+
+String.create_colors
